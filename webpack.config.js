f84c00e16beb5a0b9a5507943b1a66e1298f1e9c
sanich123 <aovoronin.piano@gmail.com>
2021-07-26 22:43:37 +0300

    Added webpack

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public'),
+  },
+  devtool: 'source-map',
+  devServer: {
+    contentBase: path.resolve(__dirname, 'public'),
+    watchContentBase: true,
+  }
+};
