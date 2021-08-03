5e2f0233d6fc947435e4ecb896571943d7b7c085
sanich123 <aovoronin.piano@gmail.com>
2021-08-03 16:52:17 +0300

    Added total price, cities and dates in the header

--- src/view/price&trip.js
+++ src/view/price&trip.js
@@ -1,15 +1,27 @@
-// import dayjs from 'dayjs';
+import dayjs from 'dayjs';
 
-export const priceAndTrip = () => (
-  `<section class="trip-main__trip-info  trip-info">
+export const priceAndTrip = (points) => {
+
+  const totalPrice = points.slice().reduce((accumulator, it) => accumulator + it.basePrice, 0);
+  const cities = new Set(points.slice().map((it) => it.destination.name));
+  const threeCities = Array.from(cities);
+  const firstCity = threeCities[0];
+  const thirdCity = threeCities[threeCities.length - 1];
+  const secondCity = threeCities[threeCities.length / 2];
+  const fromDate = dayjs(points[0].dateFrom).format('MMMM DD');
+  const toDate = dayjs(points[points.length - 1].dateTo).format('MMMM DD');
+
+  //Не забыть, что points отсортированы в main по дате, это может измениться
+
+  return `<section class="trip-main__trip-info  trip-info">
   <div class="trip-info__main">
-    <h1 class="trip-info__title">Amsterdam — Chamonix — Geneva</h1>
+    <h1 class="trip-info__title">${firstCity} — ${secondCity} — ${thirdCity}</h1>
 
-    <p class="trip-info__dates">Mar 18&nbsp;—&nbsp;20</p>
+    <p class="trip-info__dates">${fromDate}&nbsp;—&nbsp;${toDate}</p>
   </div>
 
   <p class="trip-info__cost">
-    Total: €&nbsp;<span class="trip-info__cost-value">1230</span>
+    Total: €&nbsp;<span class="trip-info__cost-value">${totalPrice}</span>
   </p>
-</section>`
-);
+</section>`;
+};
