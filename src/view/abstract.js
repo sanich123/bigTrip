601d03f198aba29541a70f0acefeeff6482101a0
sanich123 <aovoronin.piano@gmail.com>
2021-08-10 20:52:07 +0300

    Added abstract

---
+++ src/view/abstract.js
@@ -0,0 +1,25 @@
+import { createElement } from '../view/renderingUtils.js';
+export default class Abstract {
+  constructor() {
+    if (new.target === Abstract) {
+      throw new Error('Can\'t instantiate Abstract, only concrete one.');
+    }
+    this._element = null;
+  }
+
+  getTemplate() {
+    throw new Error('Abstract method not implemented: getTemplate');
+  }
+
+  getElement() {
+    if (!this._element) {
+      this._element = createElement(this.getTemplate());
+    }
+
+    return this._element;
+  }
+
+  removeElement() {
+    this._element = null;
+  }
+}
