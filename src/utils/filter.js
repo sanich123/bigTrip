ba75bbd62ae64ec6a7f69ee9de1e577c01ab80b1
sanich123 <aovoronin.piano@gmail.com>
2021-09-07 10:23:02 +0300

    Допилил фильтры

--- src/utils/filter.js
+++ src/utils/filter.js
@@ -1,11 +1,14 @@
 import { FilterType } from '../utils/constants.js';
 import dayjs from 'dayjs';
 
+export const getCurrentDates = (dateFrom, dateTo) => dayjs(dateFrom) <= dayjs() && dayjs(dateTo) >= dayjs();
+
 export const getFutureDate = (dateFrom) => dayjs(dateFrom) >= dayjs();
+
 export const getPastDate = (dateTo) => dayjs(dateTo) <= dayjs();
 
 export const filter = {
   [FilterType.EVERYTHING]: (points) => points,
-  [FilterType.FUTURE]: (points) => points.filter((point) => getFutureDate(point.dateFrom)),
-  [FilterType.PAST]: (points) => points.filter((point) => getPastDate(point.dateTo)),
+  [FilterType.FUTURE]: (points) => points.filter((point) => getFutureDate(point.dateFrom) || getCurrentDates(point.dateFrom, point.dateTo)),
+  [FilterType.PAST]: (points) => points.filter((point) => getPastDate(point.dateTo) || getCurrentDates(point.dateFrom, point.dateTo)),
 };
